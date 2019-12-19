bde8535448db7433972ad407a938dc86349d8d97
nataliamedinat <nataliamedinabotero@gmail.com>
2019-12-19 00:10:36 +0000

    Point six, just the beggining

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,24 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Text, Binary, Column
+
+Base = declarative_base()
 
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
+       Class A ttributes:
+       id : Represents a column containing a unique string, 60 chars, not null, primry key
+       created_at : Represents a column containing a datetime
+       updated_at : Represents a column containing a datetime
     """
+    id = Column(String(60), nullable=False, primary_key=True, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -29,7 +41,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -48,6 +59,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -60,3 +72,8 @@ class BaseModel:
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
+
+    def delete(self):
+        """ Delete the current instance from the storage
+        """
+        models.storage.delete()
