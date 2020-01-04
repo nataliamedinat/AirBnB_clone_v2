4563ed0e4b369f97b701fb8a7991c61fafde6dfc
nataliamedinat <nataliamedinabotero@gmail.com>
2020-01-04 02:34:18 +0000

    Prepare the web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "
+<!doctype html>
+<html>
+<head>
+<title>HTML Template example</title>
+</head>
+<body>
+</body>
+</html>
+" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "38 i \location /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo service nginx start
