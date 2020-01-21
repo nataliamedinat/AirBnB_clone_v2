e85a18a8dd03143e1cb2e16f55adc097671e7b1c
nataliamedinat <nataliamedinabotero@gmail.com>
2020-01-21 21:25:32 +0000

    Starts flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,11 +3,14 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app-strict_slashes = False
-
-app.run(host='0.0.0.0', port=500)
 
 
-@app.route('/')
+@app.route('/', strict_slashes = False)
 def hbnb():
+    """ Flask web app """
     return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=500)
+
