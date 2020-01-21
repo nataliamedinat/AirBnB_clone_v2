67bffa2b9b68fa7c5c5c1cdd8123abc7beed5e95
nataliamedinat <nataliamedinabotero@gmail.com>
2020-01-21 22:16:05 +0000

    Flask:Display C  followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ Flask web app """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    """ Flask """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_isfun():
+    """ Display c followed by the value of the text variable """
+    return 'C %s' % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
